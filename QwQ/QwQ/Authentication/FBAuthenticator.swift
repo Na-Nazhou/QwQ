42c42c1a97fb990e295a031a7abf64f2047c0da0
dawo5010 <me@dawo.me>
2020-03-12 15:17:24 +0800

    Implemented authentication/

---
+++ QwQ/QwQ/Authentication/FBAuthenticator.swift
@@ -0,0 +1,42 @@
+//
+//  FBAuthenticator.swift
+//  QwQ
+//
+//  Created by Daniel Wong on 11/3/20.
+//
+
+import FirebaseAuth
+import FirebaseFirestore
+
+class FBAuthenticator {
+
+    func signup(name: String, email: String, password: String) {
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let result = result {
+                print("login success! \(result.user.uid)")
+            }
+        }
+    }
+
+    func login(email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let result = result {
+                print("login success! \(result.user.uid)")
+            }
+        }
+    }
+
+    private func createUserInfo(name: String, email: String) {
+        let db = Firestore.firestore()
+        db.collection("customers").addDocument(data: ["name": name, "email:": email]) { (error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+
+}
---
+++ QwQ/QwQ/Storage/FBQueueStorage.swift
@@ -0,0 +1,8 @@
+//
+//  FBQueueStorage.swift
+//  QwQ
+//
+//  Created by Daniel Wong on 12/3/20.
+//
+
+import Foundation
