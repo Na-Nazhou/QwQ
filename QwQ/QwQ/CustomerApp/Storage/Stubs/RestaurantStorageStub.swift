8cd402c26166a72be7c9b835b0c3b2cbcabb591e
kxyoke <kxyoke@gmail.com>
2020-03-13 08:52:03 +0800

    Make restaurant storage stub conform to protocol.

--- QwQ/QwQ/CustomerApp/Storage/Stubs/RestaurantStorageStub.swift
+++ QwQ/QwQ/CustomerApp/Storage/Stubs/RestaurantStorageStub.swift
@@ -1,5 +1,16 @@
 class RestaurantStorageStub: RestaurantStorage {
-    func loadOpenRestaurants() -> [Restaurant] {
-        []
+    weak var logicDelegate: RestaurantStorageSyncDelegate?
+
+    func restaurantDidOpenQueue(restaurant: Restaurant) {
+        logicDelegate?.restaurantDidOpenQueue(restaurant: restaurant)
+    }
+
+    func restaurantDidCloseQueue(restaurant: Restaurant) {
+        logicDelegate?.restaurantDidOpenQueue(restaurant: restaurant)
+    }
+
+    func loadAllRestaurants() -> [Restaurant] {
+        //firebase load all restaurants
+        return []
     }
 }
