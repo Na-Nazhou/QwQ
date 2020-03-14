2d55fbde0aa7cc957d91f3129b05bad71a694fec
aliciatxl <example@mail.com>
2020-03-14 13:53:17 +0800

    Add ui for restaurant app

---
+++ QwQ-restaurant/QwQ-restaurant/Others/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  QwQ-restaurant
+//
+//  Created by Tan Su Yee on 14/3/20.
+//  Copyright © 2020 Appfish. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    
+    // MARK: Tab bar settings
+    static let barTintColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+    static let tintColor = UIColor(red: 244/255, green: 107/255, blue: 116/255, alpha: 1)
+}
