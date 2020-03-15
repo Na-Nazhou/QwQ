da3841e32755834b2e318d4ed39ae88c450d1c86
kxyoke <kxyoke@gmail.com>
2020-03-15 18:10:38 +0800

    Resolve conflict from previous merge.

--- QwQ/QwQ/Models/SharedModels/Restaurant.swift
+++ QwQ/QwQ/Models/SharedModels/Restaurant.swift
@@ -2,8 +2,8 @@ struct Restaurant: User {
     let uid: String
     let name: String
     let email: String
+    let menu: String
 
-    let phone: String
     let address: String
 
     var isOpen: Bool
