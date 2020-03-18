2f2e34b96257b17697ea25bdfaf8e157186f8f34
aliciatxl <example@mail.com>
2020-03-18 17:19:03 +0800

    Modify book and queue views

---
+++ QwQ-restaurant/QwQ-restaurant/ViewControllers/BookRecordViewController.swift
@@ -0,0 +1,51 @@
+//
+//  BookRecordViewController.swift
+//  QwQ-restaurant
+//
+//  Created by Tan Su Yee on 18/3/20.
+//
+
+import UIKit
+
+class BookRecordViewController: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var contactLabel: UILabel!
+    @IBOutlet weak var groupSizeLabel: UILabel!
+    @IBOutlet weak var babyChairQuantityLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var wheelchairFriendlySwitch: UISwitch!
+    
+    var bookRecord: QueueRecord?
+    
+    @IBAction func handleAdmit(_ sender: Any) {
+        showMessage(title: Constants.admitCustomerTitle, message: Constants.admitCustomerMessage, buttonText: Constants.okayTitle)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpViews()
+    }
+    
+    @IBAction func handleBack(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private func setUpViews() {
+        nameLabel.text = bookRecord?.customer.name
+        contactLabel.text = bookRecord?.customer.contact
+        groupSizeLabel.text = String(bookRecord?.groupSize ?? 0)
+        babyChairQuantityLabel.text = String(bookRecord?.wheelchairFriendly ?? false)
+//        wheelchairFriendlySwitch. = String(bookRecord?.wheelchairFriendly)
+    }
+    
+    func showMessage(title: String, message: String, buttonText: String) {
+        let message = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let closeDialogAction = UIAlertAction(title: buttonText, style: .default)
+        message.addAction(closeDialogAction)
+        
+        self.present(message, animated: true)
+    }
+}
+
