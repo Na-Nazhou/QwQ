0379225dde65209f11c893f6d56de9caa3c68014
kxyoke <kxyoke@gmail.com>
2020-04-25 21:10:23 +0800

    Add documentation for customer app storage protocols.

--- QwQ/QwQ/Storage/Protocols/CustomerQueueStorage.swift
+++ QwQ/QwQ/Storage/Protocols/CustomerQueueStorage.swift
@@ -1,26 +1,31 @@
+/// Represents the protocol a synchronizable storage component should conform to for queues.
 protocol QueueStorageSync {
 
     // MARK: - Listeners
+    /// Register to listen to all queue records of `customer`.
     func registerListener(for customer: Customer)
 
+    /// Removes any registered listener.
     func removeListener()
 
     // MARK: - Delegates
+    /// Register `del` as a delegate of this component.
     func registerDelegate(_ del: QueueStorageSyncDelegate)
 
+    /// Unregister `del` from this component.
     func unregisterDelegate(_ del: QueueStorageSyncDelegate)
 }
 
+/// Represents the protocol a customer queue storage component should conform to.
 protocol CustomerQueueStorage: QueueStorageSync {
     // MARK: - Modifiers
-
-    /// Insert multiple queue records
+    /// Inserts multiple queue records to the collection of queue records.
     func addQueueRecords(newRecords: [QueueRecord], completion: @escaping () -> Void)
 
-    /// Update a queue record
+    /// Updates a queue record in collection from `oldRecord` to `newRecord`.
     func updateQueueRecord(oldRecord: QueueRecord, newRecord: QueueRecord,
                            completion: @escaping () -> Void)
 
-    /// Update multiple queue records
+    /// Updates multiple queue records to the records in `newRecord`.
     func updateQueueRecords(newRecords: [QueueRecord], completion: @escaping () -> Void)
 }
